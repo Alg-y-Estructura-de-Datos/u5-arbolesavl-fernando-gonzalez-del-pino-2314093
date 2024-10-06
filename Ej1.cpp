178a29626f466e5b4aa76752f42415ea28ed44e9
⠀Cuki ⠀ <2314093@ucc.edu.ar>
2024-10-06 17:15:53 -0300

    Terminado

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,78 @@
 #include <iostream>
+#include "Arbol/ArbolBinarioAVL.h"
 using namespace std;
 
-int main() {
+/*Un almacén necesita gestionar su inventario de productos. Cada producto tiene un número único de identificación (ID), y el sistema debe mantener el inventario organizado para realizar búsquedas rápidas de productos y permitir actualizaciones del stock.
+Utilizar un árbol AVL para almacenar los IDs de los productos. El árbol debe mantenerse balanceado para garantizar búsquedas rápidas.
+Crear un sistema con un menú que permita gestionar las siguientes opciones:
+1.	Insertar un código de producto.
+2.	Eliminar un código de producto.
+3.	Buscar un producto por código.
+4.	Imprimir el árbol (mostrando su estructura balanceada).
+5.	Salir del programa.
+*/
 
-    cout << "Ejercicio N° 1" << endl;
+void menu() {
+    cout << "\n---MENU DE OPCIONES---" << endl;
+    cout << "1. Insertar un codigo de producto." << endl;
+    cout << "2. Eliminar un codigo de producto." << endl;
+    cout << "3. Buscar un producto por codigo." << endl;
+    cout << "4. Imprimir el arbol (mostrando su estructura balanceada)." << endl;
+    cout << "5. Salir del programa." << endl;
+    cout << "Ingrese una opcion: ";
+}
+
+int main() {
+    ArbolBinarioAVL<int> arbol;
+    int opcion, codigo;
+
+    do {
+        menu();
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese el codigo del producto: ";
+                cin >> codigo;
+                try {
+                    arbol.put(codigo);
+                    cout << "Codigo insertado correctamente." << endl;
+                } catch (int e) {
+                    cerr << "Error, el codigo ya existe o es invalido." << endl;
+                }
+                break;
+            case 2:
+                cout << "Ingrese el codigo del producto a eliminar: ";
+                cin >> codigo;
+                try {
+                    arbol.remove(codigo);
+                    cout << "Codigo eliminado correctamente." << endl;
+                } catch (int e) {
+                    cerr << "Error, el codigo no existe o es invalido." << endl;
+                }
+                break;
+            case 3:
+                cout << "Ingrese el codigo del producto a buscar: ";
+                cin >> codigo;
+                try {
+                    if (arbol.search(codigo)) {
+                        cout << "El codigo " << codigo << " existe en el arbol." << endl;
+                    }
+                } catch (int e) {
+                    cerr << "Error, el codigo es invalido o no existe." << endl;
+                }
+                break;
+            case 4:
+                cout << "Arbol AVL:" << endl;
+                arbol.print();
+                break;
+            case 5:
+                cout << "Saliendo del programa..." << endl;
+                break;
+            default:
+                cout << "Opcion invalida." << endl;
+        }
+    } while (opcion != 5);
 
     return 0;
 }
--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,77 @@
 #include <iostream>
+#include "Arbol/ArbolBinarioAVL.h"
 using namespace std;
 
-int main() {
+/*Imagina que trabajas en una tienda que vende una variedad de productos. Cada producto tiene un código único que lo identifica y necesitas gestionar el inventario utilizando un árbol AVL para asegurarte de que el sistema esté optimizado y balanceado. Los productos se identifican únicamente por su código, y no hay productos duplicados en el inventario.
+Opciones del menú:
+1.	Agregar producto: Inserta un nuevo código de producto en el sistema.
+2.	Eliminar producto: Elimina un código de producto del sistema.
+3.	Buscar producto: Verifica si un código de producto existe en el inventario.
+4.	Mostrar inventario: Imprime el árbol AVL de productos para mostrar su estructura balanceada.
+5.	Salir: Finaliza el programa.
+*/
 
-    cout << "Ejercicio N° 2" << endl;
+void menu() {
+    cout << "\n---MENU DE OPCIONES---" << endl;
+    cout << "1. Agregar producto." << endl;
+    cout << "2. Eliminar producto." << endl;
+    cout << "3. Buscar producto." << endl;
+    cout << "4. Mostrar inventario." << endl;
+    cout << "5. Salir." << endl;
+    cout << "Ingrese una opcion: ";
+}
+
+int main() {
+    ArbolBinarioAVL<int> arbol;
+    int opcion, codigo;
+
+    do {
+        menu();
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese el codigo del producto: ";
+            cin >> codigo;
+            try {
+                arbol.put(codigo);
+                cout << "Codigo agregado correctamente." << endl;
+            } catch (int e) {
+                cerr << "Error, el codigo ya existe o es invalido." << endl;
+            }
+            break;
+            case 2:
+                cout << "Ingrese el codigo del producto a eliminar: ";
+            cin >> codigo;
+            try {
+                arbol.remove(codigo);
+                cout << "Codigo eliminado correctamente." << endl;
+            } catch (int e) {
+                cerr << "Error, el codigo no existe o es invalido." << endl;
+            }
+            break;
+            case 3:
+                cout << "Ingrese el codigo del producto a buscar: ";
+            cin >> codigo;
+            try {
+                if (arbol.search(codigo)) {
+                    cout << "Producto " << codigo << " encontrado en el arbol." << endl;
+                }
+            } catch (int e) {
+                cerr << "Error, el codigo es invalido o no existe." << endl;
+            }
+            break;
+            case 4:
+                cout << "Arbol AVL:" << endl;
+            arbol.print();
+            break;
+            case 5:
+                cout << "Saliendo del programa..." << endl;
+            break;
+            default:
+                cout << "Opcion invalida." << endl;
+        }
+    } while (opcion != 5);
 
     return 0;
 }
